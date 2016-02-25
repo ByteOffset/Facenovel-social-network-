4a338b12867a65670c0d652fe2b7ee2a1ef43291
nutmeat <ptown77@gmail.com>
2016-02-25 09:26:23 -0800

    Moved code out of the main method in SocialNetworkDriver.java and into new method "addUser()" in SocialNetwork.java. Created skeleton of method "logIn()" that will prompt for username and password. Also renamed the social network "socialNet".

---
+++ SocialNetworkDriver.java
@@ -0,0 +1,23 @@
+package socialnet;
+
+import java.util.Scanner;
+
+/**
+ * Created by preston.peterson651 on 12/2/15.
+ */
+public class SocialNetworkDriver {
+    public static void main(String[] args) {
+
+        SocialNetwork socialNet = new SocialNetwork(); //load existing user profiles
+
+//        String name = "Preston Peterson"; //hard coded for testing
+//        String phoneNumber = "7074435555"; //hard coded for testing
+//        String residence = "Santa Paula".toLowerCase(); //hard coded for testing
+
+
+
+
+//
+
+    }
+}
